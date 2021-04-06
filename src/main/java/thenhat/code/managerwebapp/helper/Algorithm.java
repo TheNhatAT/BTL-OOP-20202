47e4668552e21fa212ba1bce5749a8f8c3f92d57
Nguyen The Nhat <nhat0103st@gmail.com>
2021-04-07 04:40:25 +0700

    them 2 interface ve xu ly thuat toan va kiem tra trung lap du lieu

---
+++ src/main/java/thenhat/code/managerwebapp/helper/Algorithm.java
@@ -0,0 +1,7 @@
+package thenhat.code.managerwebapp.helper;
+
+//== thuật toán xử lý xung đột thời gian ==
+//-- phân tập giảng viên ứng với lịch thi (theo bộ môn) -> xử lý từng tập nhỏ --
+public interface Algorithm {
+
+}
---
+++ src/main/java/thenhat/code/managerwebapp/helper/CheckDuplicateData.java
@@ -0,0 +1,5 @@
+package thenhat.code.managerwebapp.helper;
+
+//== check và ngăn chặn sự trùng lặp dữ liệu khi import (đặc biệt khi import bằng file) ==
+public interface CheckDuplicateData {
+}
