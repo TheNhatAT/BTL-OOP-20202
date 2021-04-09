279014fd5ef5e349b84c25ff71698bcaaf04cd06
Nguyen The Nhat <nhat0103st@gmail.com>
2021-04-10 06:10:21 +0700

    add simple example of spring security

--- src/main/java/thenhat/code/managerwebapp/config/WebConfig.java
+++ src/main/java/thenhat/code/managerwebapp/config/WebConfig.java
@@ -1,19 +1,12 @@
 package thenhat.code.managerwebapp.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
@@ -25,28 +18,4 @@ public class WebConfig extends WebSecurityConfigurerAdapter implements WebMvcCon
         //use to format the entity(trường hợp class trong class)
     }
 
-    //== config for security ==\\
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().anyRequest().authenticated()
-                .and().httpBasic();
-    }
-
-    //== config admin account ==\\
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder authentication)
-            throws Exception
-    {
-        authentication.inMemoryAuthentication()
-                .withUser("admin")
-                .password(passwordEncoder().encode("admin"))
-                .authorities("ROLE_USER");
-    }
-
-    //-- tim hieu them --\\
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 }
