29b3d115f8c003eded1a5db92534a5076a625d36
Nguyen The Nhat <nhat0103st@gmail.com>
2021-05-26 15:47:03 +0700

    add static files to final version

---
+++ src/main/java/thenhat/code/managerwebapp/model/entity/Assigment.java
@@ -0,0 +1,13 @@
+package thenhat.code.managerwebapp.model.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@NoArgsConstructor
+public class Assigment {
+    private Long schedules_id;
+    private Long canbo1;
+    private Long canbo2;
+}
